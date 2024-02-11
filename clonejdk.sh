929bd2e64d92843e7b685e70e840d168f0bc8ff8
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-02-11 18:56:00 +0800

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-git clone -b jdk-17.0.11+1_adopt --depth 1 https://github.com/adoptium/jdk17u openjdk
+git clone -b 20240211 --depth 1 https://github.com/aaaapai/jdk17u openjdk
