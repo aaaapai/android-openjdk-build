610d59c0efe02307e020d68532845e1ba72a1bd6
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-05-09 17:45:02 +0800

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -3,7 +3,7 @@ set -e
 . setdevkitpath.sh
 
 export FREETYPE_DIR=$PWD/freetype-$BUILD_FREETYPE_VERSION/build_android-$TARGET_SHORT
-export CUPS_DIR=$PWD/cups-2.4.7
+export CUPS_DIR=$PWD/cups-${BUILD_FREETYPE_VERSION}
 export CFLAGS+=" -DLE_STANDALONE -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-run-dce" # -I$FREETYPE_DIR -I$CUPS_DI
 if [[ "$TARGET_JDK" == "arm" ]]
 then
