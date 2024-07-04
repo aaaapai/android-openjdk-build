939908f8c95eef6453b0b8e8657932fd5750767e
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-07-04 22:37:35 +0800

    Update setdevkitpath.sh

--- setdevkitpath.sh
+++ setdevkitpath.sh
@@ -2,11 +2,6 @@
 # https://github.com/PojavLauncherTeam/openjdk-multiarch-jdk8u/blob/aarch64-shenandoah-jdk8u272-b10/jdk/src/share/native/sun/java2d/loops/GraphicsPrimitiveMgr.c
 export NDK_VERSION=r25c
 
-if [[ -z "$BUILD_FREETYPE_VERSION" ]]
-then
-  export BUILD_FREETYPE_VERSION="2.13.2"
-fi
-
 if [[ -z "$JDK_DEBUG_LEVEL" ]]
 then
   export JDK_DEBUG_LEVEL=release
@@ -49,15 +44,17 @@ export thecc=$TOOLCHAIN/bin/${TARGET}${API}-clang
 export thecxx=$TOOLCHAIN/bin/${TARGET}${API}-clang++
 
 # Configure and build.
+export DLLTOOL=/usr/bin/llvm-dlltool-18
+export CXXFILT=$TOOLCHAIN/bin/llvm-cxxfilt
 export NM=$TOOLCHAIN/bin/llvm-nm
 export CC=$PWD/android-wrapped-clang
 export CXX=$PWD/android-wrapped-clang++
 export AR=$TOOLCHAIN/bin/llvm-ar
 export AS=$TOOLCHAIN/bin/llvm-as
-export LD=$TOOLCHAIN/bin/ld
+export LD=$TOOLCHAIN/bin/ld.lld
 export OBJCOPY=$TOOLCHAIN/bin/llvm-objcopy
+export OBJDUMP=$TOOLCHAIN/bin/llvm-objdump
 export READELF=$TOOLCHAIN/bin/llvm-readelf
 export RANLIB=$TOOLCHAIN/bin/llvm-ranlib
 export STRIP=$TOOLCHAIN/bin/llvm-strip
 export LINK=$TOOLCHAIN/bin/llvm-link
-
