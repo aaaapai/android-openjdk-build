defb011b9e29c210255051b579843c6aedeb36b4
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-07-22 13:43:19 +0800

    Update tarjdk.sh

--- tarjdk.sh
+++ tarjdk.sh
@@ -32,7 +32,7 @@ cp -rv jre_override/lib/* jdkout/lib/ || true
 cd jreout
 
 # Strip
-find ./ -name '*.so' -execdir ${TOOLCHAIN}/bin/llvm-strip {} \;
+find ./ -name '*' -execdir ${TOOLCHAIN}/bin/llvm-strip {} \;
 
 tar cJf ../jre21-${TARGET_SHORT}-`date +%Y%m%d`-${JDK_DEBUG_LEVEL}.tar.xz .
 
