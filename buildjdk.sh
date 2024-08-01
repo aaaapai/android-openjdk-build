e5fb99b196fe1d48a5938d940d3325d8a92fd7e0
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-08-01 17:29:53 +0800

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -75,7 +75,7 @@ git apply --reject --whitespace=fix ../patches/jdk21u_android.diff || echo "git 
 #   --with-extra-cflags="$CPPFLAGS" \
 
 bash ./configure \
-    --with-version-pre=- \
+    --with-version-pre=" " \
     --openjdk-target=$TARGET \
     --with-extra-cflags="$CFLAGS" \
     --with-extra-cxxflags="$CFLAGS" \
