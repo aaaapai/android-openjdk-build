a3ebc56496dba6a1563f5737b41078c27015858a
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-08-07 10:45:47 +0800

    Update ci_build_global.sh

--- ci_build_global.sh
+++ ci_build_global.sh
@@ -5,12 +5,12 @@ set -e
 export JDK_DEBUG_LEVEL=release
 
   if [[ -d "$ANDROID_NDK_HOME" ]]; then
+    export ANDROID_NDK_HOME=/usr/local/lib/android/sdk/ndk/26.3.11579264
     echo "NDK already exists: $ANDROID_NDK_HOME"
   else
     echo "Downloading NDK"
     wget -nc -nv -O android-ndk-$NDK_VERSION-linux-x86_64.zip "https://dl.google.com/android/repository/android-ndk-$NDK_VERSION-linux.zip"
     unzip -q android-ndk-$NDK_VERSION-linux-x86_64.zip
-    export ANDROID_NDK_HOME=/usr/local/lib/android/sdk/ndk/26.3.11579264
   fi
 cp devkit.info.${TARGET_SHORT} ${TOOLCHAIN}
 
