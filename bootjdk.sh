71ef82b1f3485517597fbd03936803b135a963d8
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-08-17 20:48:32 +0800

    Create bootjdk.sh

---
+++ bootjdk.sh
@@ -0,0 +1,2 @@
+wget https://dragonwell.oss-cn-shanghai.aliyuncs.com/8.15.16/Alibaba_Dragonwell_Extended_8.15.16_aarch64_linux.tar.gz -O ./dragonwell8.tar.gz
+tar zxf ./dragonwell8.tar.gz -C ./dragonwell8
