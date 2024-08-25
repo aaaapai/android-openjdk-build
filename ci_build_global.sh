47d3e994440f558376f79852fef723f9941d0f44
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-08-25 14:29:26 +0800

    Update ci_build_global.sh

--- ci_build_global.sh
+++ ci_build_global.sh
@@ -10,6 +10,8 @@ wget -nc -nv -O android-ndk-$NDK_VERSION-linux-x86_64.zip "https://dl.google.com
 
 # Some modifies to NDK to fix
 
+chmod +x ./bootjdk.sh
+
 ./bootjdk.sh
 ./getlibs.sh
 ./buildlibs.sh
