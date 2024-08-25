2b6499ccaaea9ce1e1d9ab9392bcc30c5836e5ba
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-08-25 14:35:44 +0800

    Update bootjdk.sh

--- bootjdk.sh
+++ bootjdk.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
+mkdir dragonwell8
 wget https://github.com/dragonwell-project/dragonwell8/releases/download/dragonwell-extended-8.19.20_jdk8u412-ga/Alibaba_Dragonwell_Extended_8.19.20_x64_linux.tar.gz -O ./dragonwell8.tar.gz
 tar zxf ./dragonwell8.tar.gz -C ./dragonwell8
