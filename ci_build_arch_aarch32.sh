838721add7383bbba7486f3e4341e7a2c7eb0754
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-09-03 01:01:11 +0800

    Update ci_build_arch_aarch32.sh

--- ci_build_arch_aarch32.sh
+++ ci_build_arch_aarch32.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-export TARGET=arm-linux-androideabi
+export TARGET=armv7a-linux-androideabi
+export TARGET_2=arm-linux-androideabi
 export TARGET_JDK=arm
-export NDK_PREBUILT_ARCH=/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/arm-linux-androideabi/bin/strip
 
 bash ci_build_global.sh
-
