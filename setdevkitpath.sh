f7c4902d926869b31d1c9c80745a87294c84ea05
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-09-03 00:55:16 +0800

    Update setdevkitpath.sh

--- setdevkitpath.sh
+++ setdevkitpath.sh
@@ -1,10 +1,10 @@
-# Use the old NDK r10e to not get internal compile error at
+# Use the old NDK r10e to not get internal compile error at (still?)
 # https://github.com/PojavLauncherTeam/openjdk-multiarch-jdk8u/blob/aarch64-shenandoah-jdk8u272-b10/jdk/src/share/native/sun/java2d/loops/GraphicsPrimitiveMgr.c
-export NDK_VERSION=r10e
+export NDK_VERSION=r26d
 
 if [[ -z "$BUILD_FREETYPE_VERSION" ]]
 then
-  export BUILD_FREETYPE_VERSION="2.13.2"
+  export BUILD_FREETYPE_VERSION="2.13.3"
 fi
 
 if [[ -z "$JDK_DEBUG_LEVEL" ]]
@@ -19,40 +19,47 @@ else
   export TARGET_SHORT=$TARGET_JDK
 fi
 
-if [[ "$TARGET_JDK" == "aarch32" ]] || [[ "$TARGET_JDK" == "arm" ]] || [[ "$TARGET_JDK" == "x86" ]]
+if [[ -z "$JVM_VARIANTS" ]]
 then
-  echo "VM variant: client"
-  if [[ -z "$JVM_VARIANTS" ]]
-  then
-    export JVM_VARIANTS=client
-  fi
-else
-  echo "VM variant: server"
-  if [[ -z "$JVM_VARIANTS" ]]
-  then
-    export JVM_VARIANTS=server
-  fi
+  export JVM_VARIANTS=server
 fi
 
 export JVM_PLATFORM=linux
 # Set NDK
-export API=21
-export NDK=`pwd`/android-ndk-$NDK_VERSION
-export ANDROID_NDK_ROOT=$NDK 
-export TOOLCHAIN=$NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain
-# export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/linux-x86_64
+export API=22
+
+# Runners usually ship with a recent NDK already
+if [[ -z "$ANDROID_NDK_HOME" ]]
+then
+  export ANDROID_NDK_HOME=$PWD/android-ndk-$NDK_VERSION
+fi
+
+export TOOLCHAIN=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64
 
 export ANDROID_INCLUDE=$TOOLCHAIN/sysroot/usr/include
 
 export CPPFLAGS="-I$ANDROID_INCLUDE -I$ANDROID_INCLUDE/$TARGET" # -I/usr/include -I/usr/lib
-export LDFLAGS="-L$NDK/platforms/android-$API/arch-$TARGET_SHORT/usr/lib"
+if [[ "$TARGET_JDK" == "arm" ]]
+then
+  export LDFLAGS="-L$TOOLCHAIN/sysroot/usr/lib/${TARGET_2}/${API}"
+else
+  export LDFLAGS="-L$TOOLCHAIN/sysroot/usr/lib/${TARGET}/${API}"
+fi
+export thecc=$TOOLCHAIN/bin/${TARGET}${API}-clang
+export thecxx=$TOOLCHAIN/bin/${TARGET}${API}-clang++
 
 # Configure and build.
-export AR=$TOOLCHAIN/bin/$TARGET-ar
-export AS=$TOOLCHAIN/bin/$TARGET-as
-export CC=$TOOLCHAIN/bin/$TARGET-gcc
-export CXX=$TOOLCHAIN/bin/$TARGET-g++
-export LD=$TOOLCHAIN/bin/$TARGET-ld
-export OBJCOPY=$TOOLCHAIN/bin/$TARGET-objcopy
-export RANLIB=$TOOLCHAIN/bin/$TARGET-ranlib
-export STRIP=$TOOLCHAIN/bin/$TARGET-strip
+export DLLTOOL=/usr/bin/llvm-dlltool-18
+export CXXFILT=$TOOLCHAIN/bin/llvm-cxxfilt
+export NM=$TOOLCHAIN/bin/llvm-nm
+export CC=$PWD/android-wrapped-clang
+export CXX=$PWD/android-wrapped-clang++
+export AR=$TOOLCHAIN/bin/llvm-ar
+export AS=$TOOLCHAIN/bin/llvm-as
+export LD=$TOOLCHAIN/bin/ld.lld
+export OBJCOPY=$TOOLCHAIN/bin/llvm-objcopy
+export OBJDUMP=$TOOLCHAIN/bin/llvm-objdump
+export READELF=$TOOLCHAIN/bin/llvm-readelf
+export RANLIB=$TOOLCHAIN/bin/llvm-ranlib
+export STRIP=$TOOLCHAIN/bin/llvm-strip
+export LINK=$TOOLCHAIN/bin/llvm-link
