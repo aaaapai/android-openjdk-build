832cb882426b18c5384defbfb77688f5e3cecd98
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-09-21 19:08:49 +0800

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -74,8 +74,8 @@ bash ./configure \
     --with-version-pre=- \
     --target=$TARGET \
     --host=$TARGET \
-    --with-toolchain-path=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin
-    --with-sysroot=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/sysroot
+    --with-toolchain-path=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin \
+    --with-sysroot=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/sysroot \
     --openjdk-target=$TARGET \
     --with-extra-cflags="$CFLAGS" \
     --with-extra-cxxflags="$CFLAGS" \
