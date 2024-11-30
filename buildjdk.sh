584a00fdc83a09d224fa97b8f4d0ededa4f37116
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-11-30 22:07:17 +0800

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -2,7 +2,9 @@
 set -e
 . setdevkitpath.sh
 
+unset LD
 export LD=$TOOLCHAIN/bin/lld
+
 export FREETYPE_DIR=$PWD/freetype-$BUILD_FREETYPE_VERSION/build_android-$TARGET_SHORT
 export CUPS_DIR=$PWD/cups
 export CFLAGS+=" -DLE_STANDALONE -Wno-int-conversion -Wno-error=implicit-function-declaration" # -I$FREETYPE_DIR -I$CUPS_DI
