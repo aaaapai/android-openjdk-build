5bb8af17c4f6411039da218d9f81498f67c35c50
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-12-29 02:05:13 +0800

    Update tarjdk.sh

--- tarjdk.sh
+++ tarjdk.sh
@@ -26,19 +26,19 @@ exit 1
 findexec jreout | xargs -- ./termux-elf-cleaner/build/termux-elf-cleaner
 findexec jdkout | xargs -- ./termux-elf-cleaner/build/termux-elf-cleaner
 
-cp -rv jre_override/lib/* jreout/lib/ || true
+# cp -rv jre_override/lib/* jreout/lib/ || true
 cp -rv jre_override/lib/* jdkout/lib/ || true
 
-cd jreout
+# cd jreout
 
 # Strip
-find ./ -name '*' -execdir ${TOOLCHAIN}/bin/llvm-strip {} \;
+# find ./ -name '*' -execdir ${TOOLCHAIN}/bin/llvm-strip {} \;
 
-tar cJf ../jre25-${TARGET_SHORT}-`date +%Y%m%d`-${JDK_DEBUG_LEVEL}.tar.xz .
+# tar cJf ../jre25-${TARGET_SHORT}-`date +%Y%m%d`-${JDK_DEBUG_LEVEL}.tar.xz .
 
 cd ../jdkout
 tar cJf ../jdk25-${TARGET_SHORT}-`date +%Y%m%d`-${JDK_DEBUG_LEVEL}.tar.xz .
 
 # Remove jreout and jdkout
 cd ..
-rm -rf jreout jdkout
+rm -rf jdkout # jreout
