78d00448ded4eb4effd92a72e4681cd425d4a9eb
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-12-28 22:05:39 +0800

    我丟，这么快jdk25了，zulu都还没呢

--- tarjdk.sh
+++ tarjdk.sh
@@ -34,10 +34,10 @@ cd jreout
 # Strip
 find ./ -name '*' -execdir ${TOOLCHAIN}/bin/llvm-strip {} \;
 
-tar cJf ../jre24-${TARGET_SHORT}-`date +%Y%m%d`-${JDK_DEBUG_LEVEL}.tar.xz .
+tar cJf ../jre25-${TARGET_SHORT}-`date +%Y%m%d`-${JDK_DEBUG_LEVEL}.tar.xz .
 
 cd ../jdkout
-tar cJf ../jdk24-${TARGET_SHORT}-`date +%Y%m%d`-${JDK_DEBUG_LEVEL}.tar.xz .
+tar cJf ../jdk25-${TARGET_SHORT}-`date +%Y%m%d`-${JDK_DEBUG_LEVEL}.tar.xz .
 
 # Remove jreout and jdkout
 cd ..
