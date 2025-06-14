e91eafcff536d07bac657add380299262b9b0185
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-06-14 19:55:21 +0800

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -53,7 +53,7 @@ AUTOCONF_EXTRA_ARGS+="OBJCOPY=$OBJCOPY \
 export CFLAGS+=" -DANDROID -D__ANDROID__=1 -DLE_STANDALONE -Wno-int-conversion -Wno-error=implicit-function-declaration"
 
 export CFLAGS+=" -O3 -mllvm -hot-cold-split=true -fwhole-program-vtables -fdata-sections -ffunction-sections -fmerge-all-constants -ftree-vectorize -fomit-frame-pointer -fvectorize -fslp-vectorize -fno-semantic-interposition -fopenmp -pipe -integrated-as"
-export LDFLAGS+=" -fuse-ld=lld -Wl,--strip-all -fvisibility=hidden -Wl,-Bsymbolic -Wl,-O3 -Wl,--sort-common -Wl,--relax -Wl,--gc-sections -Wl,--as-needed -fopenmp -l:libomp.a"
+export LDFLAGS+=" -fuse-ld=lld -Wl,--strip-all -fvisibility=hidden -Wl,-Bsymbolic -Wl,-O3 -Wl,--sort-common -Wl,--relax -Wl,--gc-sections -Wl,--as-needed -l:libomp.a"
 #LTO
 export CFLAGS+=" -flto=auto -fno-emulated-tls"
 export LDFLAGS+=" -flto=auto -Wl,--lto-O3 -Wl,-plugin-opt=-emulated-tls=0"
