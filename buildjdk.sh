7b5131919e8b458a13127db54f8e5146df48231c
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-06-21 10:52:39 +0800

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -58,8 +58,8 @@ export LDFLAGS+=" -fuse-ld=lld -Wl,--strip-all -fvisibility=hidden -Wl,-Bsymboli
 export CFLAGS+=" -flto=auto -fno-emulated-tls"
 export LDFLAGS+=" -flto=auto -Wl,--lto-O3 -Wl,-plugin-opt=-emulated-tls=0"
 #polly
-export CFLAGS+=" -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-run-dce -mllvm -polly-parallel -mllvm -polly-scheduling=static -mllvm -polly-detect-keep-going -mllvm -polly-ast-use-context -mllvm -polly-omp-backend=LLVM -mllvm -polly-num-threads=4 -mllvm -polly-scheduling-chunksize=4"
-export OMP_NUM_THREADS=4
+export CFLAGS+=" -mllvm -polly -mllvm -polly-optimizer-level=2 -mllvm -polly-vectorizer=stripmine -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-run-dce -mllvm -polly-parallel -mllvm -polly-scheduling=static -mllvm -polly-detect-keep-going -mllvm -polly-ast-use-context -mllvm -polly-omp-backend=LLVM -mllvm -polly-num-threads=2 -mllvm -polly-scheduling-chunksize=2 -mllvm -polly-ignore-aliasing=true -mllvm -polly-remarks-minimal -mllvm -polly-enable-simplify -mllvm -polly-allow-unsigned -mllvm -polly-fusion -mllvm -polly-allow-nonaffine -mllvm -polly-process-unprofitable=true"
+export OMP_NUM_THREADS=2
 #fast-math
 export CFLAGS+=" -ffast-math -fno-finite-math-only -fno-signed-zeros -fno-trapping-math -fno-math-errno -freciprocal-math -fno-associative-math"
 
