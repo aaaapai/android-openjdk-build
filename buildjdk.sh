f9f7fda378e205bf24b8798d689a187e3e387911
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-13 09:01:25 +0800

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -56,7 +56,7 @@ AUTOCONF_EXTRA_ARGS+="OBJCOPY=$OBJCOPY \
 export CFLAGS+=" -DANDROID -D__ANDROID__=1 -D__TERMUX__=1 -DLE_STANDALONE -Wno-int-conversion -Wno-error=implicit-function-declaration -Wno-unused-command-line-argument"
 
 export CFLAGS+=" -O3 -mllvm -hot-cold-split=true -fdata-sections -ffunction-sections -fmerge-all-constants -ftree-vectorize -fomit-frame-pointer -fvectorize -fslp-vectorize -fno-semantic-interposition -pipe -integrated-as -pthread -stdlib=libc++"
-export LDFLAGS+=" -fuse-ld=lld -Wl,--strip-all -fvisibility=hidden -Wl,-O3 -Wl,--gc-sections -Wl,--as-needed"
+export LDFLAGS+=" -fuse-ld=lld -Wl,--strip-all -Wl,-O3 -Wl,--gc-sections -Wl,--as-needed"
 #LTO
 if [[ "$TARGET_JDK" != "arm" ]]
 then
