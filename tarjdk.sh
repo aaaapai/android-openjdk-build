1d35406178745005325c931b43f2865dd68f2b19
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-23 15:25:12 +0800

    Update tarjdk.sh

--- tarjdk.sh
+++ tarjdk.sh
@@ -23,8 +23,8 @@ exit 1
 ' sh {} \; -print
 }
 
-#findexec jreout | xargs ./termux-elf-cleaner/build/termux-elf-cleaner --api-level ${API}
-#findexec jdkout | xargs ./termux-elf-cleaner/build/termux-elf-cleaner --api-level ${API}
+findexec jreout | xargs ./termux-elf-cleaner/build/termux-elf-cleaner --api-level ${API}
+findexec jdkout | xargs ./termux-elf-cleaner/build/termux-elf-cleaner --api-level ${API}
 
 cp -rv jre_override/lib/* jreout/lib/ || true
 cp -rv jre_override/lib/* jdkout/lib/ || true
