41a6111d96b77687a6c67a507ef97da79a3431a4
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-23 19:00:50 +0800

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -55,7 +55,7 @@ AUTOCONF_EXTRA_ARGS+="OBJCOPY=$OBJCOPY \
 #no error
 export CFLAGS+=" -DANDROID -D__ANDROID__=1 -D__TERMUX__=1 -DLE_STANDALONE -Wno-int-conversion -Wno-error=implicit-function-declaration -Wno-unused-command-line-argument"
 
-export CFLAGS+=" -O3 -mllvm -hot-cold-split=true -fdata-sections -ffunction-sections -fmerge-all-constants -ftree-vectorize -fomit-frame-pointer -fvectorize -fslp-vectorize -pipe -integrated-as -pthread -stdlib=libc++"
+export CFLAGS+=" -O3 -mllvm -hot-cold-split=true -fdata-sections -ffunction-sections -fmerge-all-constants -ftree-vectorize -fvectorize -fslp-vectorize -pipe -integrated-as -pthread -stdlib=libc++"
 export LDFLAGS+=" -fuse-ld=lld -Wl,--strip-all -Wl,-O3 -Wl,--gc-sections -Wl,--as-needed"
 #LTO
 if [[ "$TARGET_JDK" != "arm" ]]
@@ -64,7 +64,7 @@ export CFLAGS+=" -flto=thin -fno-emulated-tls -fwhole-program-vtables"
 export LDFLAGS+=" -flto=thin -Wl,--lto-O3 -Wl,-plugin-opt=-emulated-tls=0"
 fi
 #polly
-export CFLAGS+=" -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-run-dce -mllvm -polly-parallel -mllvm -polly-scheduling=static -mllvm -polly-detect-keep-going -mllvm -polly-ast-use-context -mllvm -polly-num-threads=4 -mllvm -polly-scheduling-chunksize=4"
+export CFLAGS+=" -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-run-dce -mllvm -polly-detect-keep-going -mllvm -polly-ast-use-context"
 #fast-math
 # export CFLAGS+=" -ffast-math -fno-finite-math-only -fno-signed-zeros -fno-trapping-math -fno-math-errno -freciprocal-math -fno-associative-math"
 
