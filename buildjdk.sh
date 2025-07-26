8a04782b41a406d50cbd494886e205ec77f58413
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-26 19:24:25 +0800

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -55,8 +55,9 @@ AUTOCONF_EXTRA_ARGS+="OBJCOPY=$OBJCOPY \
 #no error
 export CFLAGS+=" -DANDROID -D__ANDROID__=1 -D__TERMUX__=1 -DLE_STANDALONE -Wno-int-conversion -Wno-error=implicit-function-declaration -Wno-unused-command-line-argument"
 
-export CFLAGS+=" -O3 -mllvm -hot-cold-split=true -fomit-frame-pointer -fdata-sections -ffunction-sections -fmerge-all-constants -ftree-vectorize -fvectorize -fslp-vectorize -pipe -integrated-as -fno-semantic-interposition -stdlib=libc++"
-export LDFLAGS+=" -fuse-ld=lld -Wl,--strip-all -Wl,-O3 -Wl,--gc-sections -Wl,--as-needed"
+#-fomit-frame-pointer -fno-semantic-interposition -mllvm -hot-cold-split=true
+export CFLAGS+=" -O3 -fdata-sections -ffunction-sections -fmerge-all-constants -ftree-vectorize -fvectorize -fslp-vectorize -pipe -integrated-as -stdlib=libc++"
+export LDFLAGS+=" -fuse-ld=lld -Wl,-O3 -Wl,--gc-sections -Wl,--as-needed"
 # 地域歧视
 if [[ "$API" -ge "29" ]]
 then
