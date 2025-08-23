e5604b0768222038fc2a6252c237fe3d85bf7671
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-08-23 08:18:06 +0800

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -89,7 +89,7 @@ git apply --reject --whitespace=fix ../patches/jdk26u_android.diff || echo "git 
 # if [[ "$API" == "21" ]] || [[ "$API" == "22" ]]; then
 #   git apply --reject --whitespace=fix ../patches/jdk26u_android5.diff || echo "git apply failed (Android patch set)"
 # fi
-git apply --reject --whitespace=fix ../patches/jdk26u_termux.diff || echo "git apply failed (Termux patch set)"
+# git apply --reject --whitespace=fix ../patches/jdk26u_termux.diff || echo "git apply failed (Termux patch set)"
 
 bash ./configure \
     --with-version-pre="-ea" \
