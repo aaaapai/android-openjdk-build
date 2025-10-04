8b565605a3c90cc2265a0edcfb2c2c5f30a952ae
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-10-04 15:06:35 +0800

    Update removejdkdebuginfo.sh

--- removejdkdebuginfo.sh
+++ removejdkdebuginfo.sh
@@ -10,6 +10,8 @@ mkdir -p dizout dSYM-temp/{lib,bin}
 
 cp freetype-$BUILD_FREETYPE_VERSION/build_android-$TARGET_SHORT/lib/libfreetype.so $targetpath/images/jdk/lib/
 
+bash ./graal_build.bash
+
 cp -r $targetpath/images/jdk jdkout
 
 # JDK no longer create separate JRE image, so we have to create one manually.
