58559c99237522a401684456df1cf102894cbc39
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-10-04 15:09:12 +0800

    Update graal_build.bash

--- graal_build.bash
+++ graal_build.bash
@@ -1,4 +1,11 @@
+#!/bin/bash
 set -e
+. setdevkitpath.sh
 
-cd ../graalvm/substratevm
-$MX_PATH/mx --java-home=$JAVA_HOME build
+cd ./graalvm
+
+# 用 mx 工具构建 GraalVM 组件
+$MX_PATH/mx --primary-suite-path compiler --java-home=$JAVA_HOME build
+
+# 构建 native-image（或其它 GraalVM 组件）
+$MX_PATH/mx --primary-suite-path substratevm --java-home=$JAVA_HOME build
