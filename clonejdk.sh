5b7258151ffe2a33cdc9f1324b5e2efdd7e93963
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-10-04 15:18:58 +0800

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -3,5 +3,5 @@ set -e
 
 git clone -b jdk25 --depth 1 https://github.com/graalvm/labs-openjdk openjdk
 
-git clone -b master --depth 1 https://github.com/oracle/graal.git graalvm
+git clone -b master --depth 1 https://github.com/oracle/graal.git openjdk/graal
 git clone --depth 1 https://github.com/graalvm/mx.git mx
