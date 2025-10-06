12a8a037d68281ac6eb30f88c07f6111af9b6f36
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-10-06 23:54:04 +0800

    Update graal_build.bash

--- graal_build.bash
+++ graal_build.bash
@@ -5,6 +5,6 @@ set -e
 cd ./openjdk/graal
 
 # 用 mx 工具构建 GraalVM 组件
-$MX_PATH/mx --primary-suite-path compiler --java-home=../build/${JVM_PLATFORM}-${TARGET_JDK}-${JVM_VARIANTS}-${JDK_DEBUG_LEVEL}/buildjdk/jdk --tools-java-home=${JAVA_HOME} build
-$MX_PATH/mx --primary-suite-path vm --java-home=../build/${JVM_PLATFORM}-${TARGET_JDK}-${JVM_VARIANTS}-${JDK_DEBUG_LEVEL}/buildjdk/jdk --tools-java-home=${JAVA_HOME} build
+$MX_PATH/mx --primary-suite-path compiler --java-home=${JAVA_HOME} --tools-java-home=${JAVA_HOME} build
+$MX_PATH/mx --primary-suite-path vm --java-home=${JAVA_HOME} --tools-java-home=${JAVA_HOME} build
 # $MX_PATH/mx --primary-suite-path substratevm --java-home=../build/${JVM_PLATFORM}-${TARGET_JDK}-${JVM_VARIANTS}-${JDK_DEBUG_LEVEL}/buildjdk/jdk --tools-java-home=${JAVA_HOME} build
