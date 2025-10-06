54a7938119ba6df1b34487f9b0387a32d24afc45
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-10-06 20:35:48 +0800

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -53,10 +53,9 @@ AUTOCONF_EXTRA_ARGS+="OBJCOPY=$OBJCOPY \
   "
 
 #no error
-export CFLAGS+=" -DANDROID -D__ANDROID__=1 -D__TERMUX__=1 -DLE_STANDALONE -Wno-int-conversion -Wno-error=implicit-function-declaration -Wno-unused-command-line-argument -Wno-exception-specification -O1 -mllvm -polly -mllvm -polly-omp-backend=LLVM -fno-optimize-sibling-calls"
+export CFLAGS+=" -DANDROID -D__ANDROID__=1 -D__TERMUX__=1 -DLE_STANDALONE -Wno-int-conversion -Wno-error=implicit-function-declaration -Wno-unused-command-line-argument -Wno-exception-specification -O2 -mllvm -polly -mllvm -polly-omp-backend=LLVM -fno-optimize-sibling-calls"
 
 << EOF
-
 export CFLAGS+=" -O3 -fomit-frame-pointer -fno-semantic-interposition -mllvm -hot-cold-split=true -fdata-sections -ffunction-sections -fmerge-all-constants -ftree-vectorize -fvectorize -fslp-vectorize -pipe -integrated-as -stdlib=libc++"
 export LDFLAGS+=" -fuse-ld=lld -Wl,--gc-sections -Wl,-O3 -Wl,--sort-common -Wl,--as-needed -l:libomp.a"
 
