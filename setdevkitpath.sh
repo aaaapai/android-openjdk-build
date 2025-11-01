20c2df6152db32531f6bda164ad5f8a70ef54a83
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-11-02 00:53:54 +0800

    Update setdevkitpath.sh

--- setdevkitpath.sh
+++ setdevkitpath.sh
@@ -2,11 +2,6 @@
 # https://github.com/PojavLauncherTeam/openjdk-multiarch-jdk8u/blob/aarch64-shenandoah-jdk8u272-b10/jdk/src/share/native/sun/java2d/loops/GraphicsPrimitiveMgr.c
 export NDK_VERSION=r28
 
-if [[ -z "$BUILD_FREETYPE_VERSION" ]]
-then
-  export BUILD_FREETYPE_VERSION="2.14.1"
-fi
-
 if [[ -z "$JDK_DEBUG_LEVEL" ]]
 then
   export JDK_DEBUG_LEVEL=release
@@ -26,7 +21,7 @@ fi
 
 export JVM_PLATFORM=linux
 # Set NDK
-export API=26
+export API=29
 
 # Runners usually ship with a recent NDK already
 if [[ -z "$ANDROID_NDK_HOME" ]]
