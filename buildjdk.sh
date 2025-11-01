80689298c30ef9347bf26c5d52f0e87122a3271b
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-11-02 01:04:30 +0800

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -2,7 +2,7 @@
 set -e
 . setdevkitpath.sh
 
-export FREETYPE_DIR=$PWD/freetype-$BUILD_FREETYPE_VERSION/build_android-$TARGET_SHORT
+export FREETYPE_DIR=$PWD/freetype/build_android-$TARGET_SHORT
 export CUPS_DIR=$PWD/cups
 
 if [[ "$TARGET_JDK" == "arm" ]]
