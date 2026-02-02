fad08231d7bd01d839ff9f1f6d593f979e1aaa47
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-02-02 19:27:44 +0800

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -7,7 +7,7 @@ export CUPS_DIR=$PWD/cups
 
 if [[ "$TARGET_JDK" == "arm" ]]
 then
-  export CFLAGS+=" -D__thumb__ -Dfseeko=fseek -Dftello=ftell"
+  export CFLAGS+=" -D__thumb__"
 else
   if [[ "$TARGET_JDK" == "x86" ]]; then
      export CFLAGS+=" -mstackrealign"
