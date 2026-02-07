8587b1e4cb331e066c7c1518c3714d3850cb2915
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-02-07 20:13:36 +0800

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-git clone --depth 1 -b dragonwell_extended-21.0.10.0.10+7 https://github.com/dragonwell-project/dragonwell21 openjdk
+git clone --depth 1 https://github.com/aaaapai/dragonwell21 openjdk
